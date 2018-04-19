165d7ab29a7f713b228abdc40e987bea9ffd6939
Samuel Barrena Bermudez <Samuel@Mac-Pro-de-Samuel.local>
2018-04-19 11:32:04 +0200

    amplio funciones de visualización en functions del theme

--- functions.php
+++ functions.php
@@ -2,69 +2,142 @@
 include "custom_post_oshito/custom-oshito.php";
 
 $argumentos[0] = array(
-	'nombre_custom'		=>	'prueba',
-	'label'				=>	'asi se llama',
-	'taxonomia_propia'	=>	array('nombre'=>"genero", "descripcion"=>"esto sería lo que sale en el label"),
+	'nombre_custom'		=>	'bandas',
+	'editor'			=>	true,
+	'imagen_destacada'	=>	true,
+	'resumen'			=>	true,
+	'label'				=>	'Bandas',
 	'metabox'			=>	array(
-								array('nombre'=>"nombre", "tipo"=>"texto_simple", "descripcion"=>"esto sería lo que sale en el label"),
-								array('nombre'=>"nombre2", "tipo"=>"imagen", "descripcion"=>"aquí podríamos poner lo que queramos"),
-								array('nombre'=>"nombre3", "tipo"=>"texto_complejo", "descripcion"=>"aquí podríamos poner lo que queramos"),
-							)
-);
-
-
-
-$argumentos[0] = array(
-	'nombre_custom'		=>	'documentacion_femp',
-	'label'				=>	'Documentación FEMP',
-	'editor'			=>	false,
-	'imagen_destacada'	=>	false,
-	'taxonomia_propia'	=>	array('nombre'=>"tipo_documento", "descripcion"=>"Tipo de documento"),
-	'metabox'			=>	array(
-								array('nombre'=>"archivos", "tipo"=>"texto_complejo", "descripcion"=>"Archivos para descarga (formato lista)"),
+								array('nombre'=>"subtitulo", "tipo"=>"texto_simple", "descripcion"=>"Sutítulo"),
+								array('nombre'=>"web", "tipo"=>"texto_simple", "descripcion"=>"Web"),
+								array('nombre'=>"facebook", "tipo"=>"texto_simple", "descripcion"=>"Facebook"),
+								array('nombre'=>"youtube", "tipo"=>"texto_simple", "descripcion"=>"Youtube"),
+								array('nombre'=>"bandcamp", "tipo"=>"texto_simple", "descripcion"=>"Bandcamp"),
+								array('nombre'=>"instagram", "tipo"=>"texto_simple", "descripcion"=>"Instagram"),
+								array('nombre'=>"categoria_evento", "tipo"=>"texto_simple", "descripcion"=>"Categoría para la agenda de eventos"),
+								array('nombre'=>"son", "tipo"=>"texto_complejo", "descripcion"=>"La Banda son:"),
+								array('nombre'=>"discografia", "tipo"=>"texto_complejo", "descripcion"=>"Discografía"),
+								array('nombre'=>"video_destacado", "tipo"=>"texto_simple", "descripcion"=>"Enlace youtube vídeo destacado"),
 							)
 );
 
 $argumentos[1] = array(
-		'nombre_custom'		=>	'leg_municipal',
-		'label'				=>	'Legislación Municipal',
-		'editor'			=>	false,
-		'imagen_destacada'	=>	false,
-		'taxonomia_propia'	=>	array('nombre'=>"tipo_datos", "descripcion"=>"Tipo de datos"),
-		'metabox'			=>	array(
-									array('nombre'=>"archivos", "tipo"=>"texto_complejo", "descripcion"=>"Archivos para descarga (formato lista)"),
-								)
-	);
-
-
-$argumentos[2] = array(
-		'nombre_custom'		=>	'experiencias',
-		'label'				=>	'Experiencias Transformadoras',
-		'editor'			=>	false,
-		'imagen_destacada'	=>	true,
-		'metabox'			=>	array(
-									array('nombre'=>"entidad_local", "tipo"=>"texto_simple", "descripcion"=>"Entidad local"),
-									array('nombre'=>"datos", "tipo"=>"texto_lineas", "descripcion"=>"Datos"),
-									array('nombre'=>"descripcion", "tipo"=>"texto_lineas", "descripcion"=>"Descripción"),
-									array('nombre'=>"objetivos", "tipo"=>"texto_lineas", "descripcion"=>"Objetivos"),
-									array('nombre'=>"resultados", "tipo"=>"texto_lineas", "descripcion"=>"Resultados"),
-									array('nombre'=>"archivos", "tipo"=>"texto_complejo", "descripcion"=>"Documentos Adjuntos (formato lista)"),
-								)
-	);
-
-
+	'nombre_custom'		=>	'event',
+	'label'				=>	'Fechas',
+	'metabox'			=>	array(
+								array('nombre'=>"entradas", "tipo"=>"texto_simple", "descripcion"=>"Entradas"),
+								array('nombre'=>"masinfo", "tipo"=>"texto_simple", "descripcion"=>"Más Información"),
+							)
+);
 
 crear_custom($argumentos);
 
 function elemento_listado_custom($nombre){
 	?>
-	<div class="contenedor">
+	<div class="contenedor" onclick="location.href='<? the_permalink(); ?>';" >
 		<div class="imagen"><?php the_post_thumbnail();?></div>
 		<div class="titulo"><h2><? the_title(); ?></h2></div>
-		<div class="contenido"><? the_content(); ?></div>
 	</div>
 	<?
     }
+
+add_shortcode('listar_custom', 'listar_custom_funcion');
+
+function filterEventOutputCondition($replacement, $condition, $match, $EM_Event){
+    if (is_object($EM_Event)) {
+
+        switch ($condition) {
+
+            // replace LF with HTML line breaks
+            case 'nl2br':
+                // remove conditional
+                $replacement = preg_replace('/\{\/?nl2br\}/', '', $match);
+                // process any placeholders and replace LF
+                $replacement = nl2br($EM_Event->output($replacement));
+                break;
+
+            // #_ATT{Website}
+            case 'hay_entradas':
+                if (is_array($EM_Event->event_attributes) && !empty($EM_Event->event_attributes['entradas']))
+                    $replacement = preg_replace('/\{\/?hay_entradas\}/', '', $match);
+                else
+                    $replacement = '';
+                break;
+
+        }
+
+    }
+
+    return $replacement;
+}
+
+add_filter('em_event_output_condition', 'filterEventOutputCondition', 10, 4);
+
+function listar_fechas($categoria, $titulo){
+	if($categoria != ""):
+	?>
+	<div class="fechas">
+
+	<? if($titulo !=""): ?>
+		<h1><? echo $titulo; ?></h1>
+	<? endif;
+	$comunicanroll='<span class="titulo">#_EVENTNAME</span>';
+	if($categoria=="90")$comunicanroll="";
+	echo do_shortcode('
+			[events_list category="'.$categoria.'" limit="99999"]
+			<div class="contenedor_fecha">
+				{has_image}<div class="imagen">#_EVENTIMAGE</div>{/has_image}
+				<div class="fecha_izquierda">
+					<span class="dia">#d/#m</span>'.$comunicanroll.'<span class="info"><b>#_LOCATIONNAME</b> · #_LOCATIONTOWN</span>
+				</div>
+				<div class="fecha_derecha">
+					<span class="masinfo"><a href="#_ATT{masinfo}" target="_blank"><i class="fas fa-info-circle"></i></a></span>
+					{hay_entradas}
+					<span class="entradas"><a href="#_ATT{entradas}" target="_blank"><i class="fas fa-ticket-alt fa-1x"></i></a></span>
+					{/hay_entradas}
+				</div>
+			</div>
+			[/events_list]
+		');
+	?>
+
+	</div>
+
+	<?
+	endif;
+}
+
+function fechas_banda_function($atts){
+	extract(shortcode_atts(array(
+		'categoria'		=>	'',
+		'titulo'		=>	'',
+	), $atts));
+
+	ob_start();
+
+	listar_fechas($categoria, $titulo);
+
+	$posts = ob_get_contents();
+
+	ob_end_clean();
+
+	$class = " et_pb_bg_layout_{$background_layout}";
+
+	$output = sprintf(
+		'<div%5$s class="%1$s%3$s%6$s">
+			%2$s
+		%4$s',
+		( 'on' === $fullwidth ? 'et_pb_posts' : 'et_pb_blog_grid clearfix' ),
+		$posts,
+		esc_attr( $class ),
+		( ! $container_is_closed ? '</div> <!-- .et_pb_posts -->' : '' ),
+		( '' !== $module_id ? sprintf( ' id="%1$s"', esc_attr( $module_id ) ) : '' ),
+		( '' !== $module_class ? sprintf( ' %1$s', esc_attr( $module_class ) ) : '' )
+	);
+
+	return $output;
+    }
+
+add_shortcode('fechas_banda', 'fechas_banda_function');
+
 ?>
-
-
